019809061468a3aa6702294dc1fdb9c4bed10b63
Daniel Brutti <danielbrutti@gmail.com>
2020-03-13 15:17:41 -0300

    Refactoring on package structure for DDD

--- backoffice/src/main/java/org/danielbrutti/cqrs/ddd/spring/backoffice/task/infrastructure/TaskConfiguration.java
+++ backoffice/src/main/java/org/danielbrutti/cqrs/ddd/spring/backoffice/task/infrastructure/TaskConfiguration.java
@@ -3,19 +3,24 @@ package org.danielbrutti.cqrs.ddd.spring.backoffice.task.infrastructure;
 import org.danielbrutti.cqrs.ddd.spring.backoffice.task.application.TaskCreator;
 import org.danielbrutti.cqrs.ddd.spring.shared.domain.bus.event.DomainEventPublisher;
 import org.danielbrutti.cqrs.ddd.spring.shared.infrastructure.bus.event.GuavaDomainEventBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TaskConfiguration {
 
+    @Autowired
+    private TaskInMemoryRepository taskInMemoryRepository;
+
     @Bean
     public TaskCreator taskCreator() {
-        return new TaskCreator(new TaskInMemoryRepository(), domainEventPublisher());
+        return new TaskCreator(taskInMemoryRepository, domainEventPublisher());
     }
 
     @Bean
     public DomainEventPublisher domainEventPublisher() {
         return new GuavaDomainEventBus();
     }
+
 }
--- backoffice/src/test/java/org/danielbrutti/cqrs/ddd/spring/backoffice/task/infrastructure/CreateTaskIntTest.java
+++ backoffice/src/test/java/org/danielbrutti/cqrs/ddd/spring/backoffice/task/infrastructure/CreateTaskIntTest.java
@@ -3,7 +3,6 @@ package org.danielbrutti.cqrs.ddd.spring.backoffice.task.infrastructure;
 import org.danielbrutti.cqrs.ddd.spring.backoffice.task.application.TaskCreator;
 import org.danielbrutti.cqrs.ddd.spring.backoffice.task.domain.*;
 import org.danielbrutti.cqrs.ddd.spring.shared.domain.bus.event.DomainEventPublisher;
-import org.danielbrutti.cqrs.ddd.spring.shared.infrastructure.bus.event.GuavaDomainEventBus;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -44,8 +47,9 @@ public final class CreateTaskIntTest {
 
         creator.create(id, type, title, priority, description, creationDate);
 
-        verify(repository, times(1)).save(any());
-        verify(domainEventPublisher, times(1)).publish(anySet());
+        Optional<Task> task = repository.find(id);
+        assertTrue(task.isPresent());
+        assertNotNull(task.get());
 
     }
 }
